a848b9612a512568c298ebae64e5a88ccd6a05bb
stengof <lmikon007@gmail.com>
2021-03-03 23:26:09 +0300

    feat: basic functional game

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       turn_of_player: 'X',
-      board: ['', '', '', '', '', '', '', '', '']
+      board: ['', '', '', '', '', '', '', '', ''],
+      x_score: 0,
+      o_score: 0
     }
   }
 
@@ -22,26 +24,60 @@ export default class App extends React.Component {
       [2, 4, 6]
     ];
     let board = [...this.state.board];
-    board[index] = this.state.turn_of_player;
+    if (board[index] === '') {
+      board[index] = this.state.turn_of_player;
+      this.setState({
+        turn_of_player: (this.state.turn_of_player === 'X') ? 'O' : 'X',
+        board: board
+      });
+    }
 
     for (let i = 0; i < win_combos.length; i++) {
       let [w1, w2, w3] = [win_combos[i][0], win_combos[i][1], win_combos[i][2]];
       if (board[w1] !== '' && board[w1] === board[w2] && board[w2] === board[w3]) {
         // END GAME
-        alert('END GAME');
+        if (this.state.turn_of_player === 'X') {
+          this.addScore('X');
+        } else if (this.state.turn_of_player === 'O') {
+          this.addScore('O');
+        }
+        console.log(this.state)
+        this.clearBoard();
       }
     }
+    console.log(this.state)
+  }
 
-    this.setState({
-      turn_of_player: (this.state.turn_of_player === 'X') ? 'O' : 'X',
-      board: board
-    });
+  addScore(player) {
+    if (player === 'X') {
+      this.setState(state => ({
+        x_score: state.x_score + 1
+      }));
+    } else if (player === 'O') {
+      this.setState(state => ({
+        o_score: state.o_score + 1
+      }));
+    }
+
+  }
+
+  clearBoard() {
+    this.setState(state => ({
+      turn_of_player: 'X',
+      board: ['', '', '', '', '', '', '', '', ''],
+      x_score: state.x_score,
+      o_score: state.o_score
+    }))
   }
 
   render () {
     return (
       <div className='App'>
         <h1>Tic Tac Toe Game</h1>
+        <p className='scores'>
+          <span value>X scores: {this.state.x_score}</span><br />
+          <span>O scores: {this.state.o_score}</span>
+          </p>
         <div className='board'>
           {this.state.board.map((cell, index) => {
             return (
